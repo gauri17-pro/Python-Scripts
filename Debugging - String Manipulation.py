d3745eacf6a6d581e13e6539282ac43838dfcd42
gauri17-pro <60473255+gauri17-pro@users.noreply.github.com>
2023-01-18 14:57:53 +0530

    My first commit

---
+++ Debugging - String Manipulation.py
@@ -0,0 +1,4 @@
+print(Day 1: Working with strings")
+print(("Hello guys, welcome to python strings concept")
+print("Concatenation of strings can be done using "+" in python.")
+    print('For example "Hello" + "world" ')
